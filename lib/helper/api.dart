b926fecbedf6766de0bc876c973045a4c7bd746a
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-08-04 21:32:40 +0200

    add api helper

---
+++ lib/helper/api.dart
@@ -0,0 +1,4 @@
+class ApiHealper {
+  static const singIn = 'https://rayanzinotblans.000webhostapp.com/login.php';
+  static const singUp = 'https://rayanzinotblans.000webhostapp.com/singUp.php';
+}
