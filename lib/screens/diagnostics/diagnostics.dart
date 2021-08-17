db0bfdf7d4c3cb4f66531560b72f61a13af09825
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-08-17 12:36:05 +0200

    update architecture diagnostics

--- lib/screens/diagnostics/diagnostics.dart
+++ lib/screens/diagnostics/diagnostics.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:med_quizz/screens/diagnostics/widgets/diag_tile.dart';
 import 'package:med_quizz/screens/diagnostics/widgets/header_diag.dart';
 
 class Diagnostics extends StatelessWidget {
@@ -32,92 +33,7 @@ class Diagnostics extends StatelessWidget {
                       scrollDirection: Axis.vertical,
                       itemCount: 3,
                       itemBuilder: (context, index) {
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 310,
-                          margin:
-                              EdgeInsets.only(right: 20, left: 20, bottom: 20),
-                          // padding: const EdgeInsets.symmetric(vertical: 130),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black45.withOpacity(0.2),
-                                spreadRadius: 1,
-                                blurRadius: 4,
-                                offset: Offset(0.7, 0.7),
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30),
-                                        topRight: Radius.circular(30)),
-                                    image: DecorationImage(
-                                      image: AssetImage(
-                                          'assets/images/background_page.jpeg'),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(30),
-                                      bottomRight: Radius.circular(30),
-                                    ),
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        'What is Lorem Ipsum?',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 5,
-                                      ),
-                                      Text(
-                                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                                        maxLines: 6,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ), // ReadMoreText(
-                                      //   'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                                      //   trimLines: 3,
-                                      //   trimLength: 500,
-                                      //   colorClickableText: Colors.pink,
-                                      //   trimMode: TrimMode.Length,
-                                      //   trimCollapsedText: 'Show more',
-                                      //   trimExpandedText: 'Show less',
-                                      //   moreStyle: TextStyle(
-                                      //       fontSize: 13, fontWeight: FontWeight.bold),
-                                      // ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
+                        return DiagTile();
                       },
                     ),
                   ),
