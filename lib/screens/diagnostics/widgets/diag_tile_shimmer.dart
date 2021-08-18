b51e6d7be4f676f554a553356f4197ca711430e4
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-08-18 17:17:08 +0200

    update architecture diagnostics screen

---
+++ lib/screens/diagnostics/widgets/diag_tile_shimmer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class DiagTileShimmer extends StatelessWidget {
+  const DiagTileShimmer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 20, left: 20),
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Shimmer.fromColors(
+            baseColor: Colors.white,
+            highlightColor: Colors.grey.shade300,
+            child: Container(
+              margin: EdgeInsets.only(bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 4,
+                    offset: Offset(0.7, 0.7),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
